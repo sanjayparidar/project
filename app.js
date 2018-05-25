b06fa60a4ca83211300fbf5085256bb9cbccfdfb
mohhit <vermasunny09@gmail.com>
2018-05-25 16:09:35 +0530

    changes

--- app.js
+++ app.js
@@ -3,17 +3,21 @@ var app=express();
 
 app.use(require("./controller/default"));
 
+app.use(express.static(__dirname+"/public/"));
+
 var bodyparser=require("body-parser");
 app.use(bodyparser());
+
+app.set("view engine","ejs");
+app.set("views","view");
+
 
 
 app.listen(process.env.PORT||1994,function(){
 
 	console.log("server start");
-})
+});
 
-app.set("view engine","ejs");
-app.set("views","view");
 
 
 
--- model/product.js
+++ model/product.js
@@ -2,27 +2,32 @@ module.exports.getAll= [
 		{
 			productName : "Sony",
 			productPrice : 25000,
-			imgUrl : "images/1.png"
+			imgUrl : "./images/3.png"
 		},
 		{
 			productName : "HTC",
 			productPrice : 22000,
-			imgUrl : "images/2.jpg"
+			imgUrl : "./images/4.png"
 		},
 		{
 			productName : "Iphone",
 			productPrice : 57000,
-			imgUrl : "images/4.png"
+			imgUrl : "./images/1.png"
 		},
 		{
 			productName : "Samsung",
 			productPrice : 16500,
-			imgUrl : "images/3.jpg"
+			imgUrl : "./images/2.png"
 		},
 		{
 			productName : "Mi",
 			productPrice : 12500,
-			imgUrl : "images/3.jpg"
+			imgUrl : "./images/6.png"
+		},
+		{
+			productName : "one plus",
+			productPrice : 35000,
+			imgUrl : "./images/5.png"
 		}
 
 ];
